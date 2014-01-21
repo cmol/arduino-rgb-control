5684abaf0c0a7997d03f155aad3f4352fe5477e0
Claus Lensbøl <cmol@cmol.dk>
2014-01-21 17:31:36 +0100

    Added serial select

--- tcpServer.rb
+++ tcpServer.rb
@@ -1,26 +1,32 @@
 #!/usr/bin/env ruby
 require 'socket'
-require 'serialport'
+serial = (ARGV[0] == "--no-serial" ? false : true)
 
-#params for serial port
-port_str = "/dev/ttyACM3"
-baud_rate = 9600
-data_bits = 8
-stop_bits = 1
-parity = SerialPort::NONE
+if serial
+  require 'serialport'
 
-sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+  #params for serial port
+  port_str = "/dev/ttyACM3"
+  baud_rate = 9600
+  data_bits = 8
+  stop_bits = 1
+  parity = SerialPort::NONE
+
+  sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+end
 
 trap("SIGINT") {exit!}
 
 server = TCPServer.new 2000 # Server bind to port 2000
 loop do
   client = server.accept    # Wait for a client to connect
+  puts "Client connected..."
   while line = client.gets
     break if(line.chomp == "q")
-    sp.write(line.chomp)
+    sp.write(line.chomp) if serial
     puts line.chomp.unpack("C*").join(",")
 
   end
   client.close
+  puts "Client disconnected..."
 end
